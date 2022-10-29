245e5777decebfc6e57d5ab224800020606dee45
Daniel Lamando <dan@danopia.net>
2022-10-29 19:27:04 +0000

    Try supporting inline dockerfiles

--- build.danopia.net/lib/logic.ts
+++ build.danopia.net/lib/logic.ts
@@ -359,6 +359,7 @@ async function createBuildJob(buildRes: Build) {
             command: ['bash', '-euxc', `
               git clone -- "$SOURCE_CONTEXT" app
               cd app/"$CONTEXT_DIR"
+              ${buildRes.spec.source.dockerfile ? `echo "${btoa(buildRes.spec.source.dockerfile)}" | base64 -D > Dockerfile` : '# using dockerfile from repo'}
               buildah bud -t "$TARGET_IMAGE" .
               buildah push --digestfile digestfile -- "$TARGET_IMAGE"
               echo "build.danopia.net digest=$(cat digestfile)"
