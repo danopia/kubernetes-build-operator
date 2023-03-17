566dff484c1e2fc8b8f5bf9d4c3351d381649a11
Daniel Lamando <dan@danopia.net>
2023-03-17 10:14:39 +0100

    Add a tracer around the main loop

--- build.danopia.net/lib/main.ts
+++ build.danopia.net/lib/main.ts
@@ -3,9 +3,21 @@
 import { fixedInterval } from "./deps.ts";
 
 import { checkBuildConfigs, checkBuilds } from "./logic.ts";
+import { trace } from "./tracer.ts";
+
+const tracer = trace.getTracer('operator-loop');
 
 for await (const _ of fixedInterval(10 * 1000)) {
   console.log('---', new Date());
-  await checkBuildConfigs();
-  await checkBuilds();
+
+  await tracer.startActiveSpan('checkBuildConfigs', {}, span =>
+    checkBuildConfigs()
+      .catch(err => span.recordException(err))
+      .finally(() => span.end()));
+
+  await tracer.startActiveSpan('checkBuilds', {}, span =>
+    checkBuilds()
+      .catch(err => span.recordException(err))
+      .finally(() => span.end()));
+
 }
