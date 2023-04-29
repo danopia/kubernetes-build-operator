84d56c091d70610a768b6a82c84451a3d614b2d4
Daniel Lamando <dan@danopia.net>
2023-04-29 20:24:00 +0200

    Update library

--- cloudydeno.github.io/generate-api.sh
+++ cloudydeno.github.io/generate-api.sh
@@ -1,7 +1,7 @@
 #!/bin/sh -eux
 
 CodegenSrc="https://raw.githubusercontent.com/cloudydeno/deno-kubernetes_apis/main/generation/run-on-crds.ts"
-ApisImportUrl="https://deno.land/x/kubernetes_apis@v0.3.2/"
+ApisImportUrl="https://deno.land/x/kubernetes_apis@v0.4.0/"
 
 deno run --allow-read=. --allow-write=lib "$CodegenSrc" deploy/crds . "$ApisImportUrl"
 
