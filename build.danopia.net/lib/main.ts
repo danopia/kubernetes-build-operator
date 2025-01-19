9631daad740c38ee932954d85d96b43eccb9c567
Daniel Lamando <dan@danopia.net>
2025-01-20 00:49:58 +0100

    Update tracer

--- build.danopia.net/lib/main.ts
+++ build.danopia.net/lib/main.ts
@@ -1,9 +1,10 @@
 #!/usr/bin/env -S deno run --unstable --allow-all
 
+import "https://deno.land/x/observability@v0.7.0/preconfigured/from-environment.ts";
 import { fixedInterval } from "./deps.ts";
 
 import { checkBuildConfigs, checkBuilds } from "./logic.ts";
-import { trace } from "./tracer.ts";
+import { trace } from "https://deno.land/x/observability@v0.7.0/opentelemetry/api.js";
 
 const loopTracer = trace.getTracer('loop');
 
